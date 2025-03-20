36b1d04e7425cc9d373f3d2f987a3a00a718fa59
axelsormen <125052907+axelsormen@users.noreply.github.com>
2025-03-20 07:35:43 +0100

    Comments

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+######### IMPORTS & LAYOUT #########
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,6 +14,8 @@ st.set_page_config(layout="wide")
 # Display the header
 st.header("F1 Drivers Comparison 2024")
 
+######### APIS #########
+
 @st.fragment
 def api():
     st.session_state.drivers_array = get_drivers_data()
@@ -19,46 +23,50 @@ def api():
     st.session_state.qualifying_array = get_qualifying_data()
 api()
 
+######### MAIN #########
+
 @st.fragment
 def main():
-    # Add a multiselect widget to allow users to choose drivers
+
+    ######### MULTISELECT #########
+
     selected_drivers = st.multiselect(
         'Select drivers to compare:', 
         [f"{driver['given_name']} {driver['family_name']}" for driver in st.session_state.drivers_array]
-    )   
-                                
+    )  
+
+    ######### STANDINGS #########
+                      
     selected_standings_info = []
 
-    # Iterate through the selected drivers
     for selected in selected_drivers:
-        selected = selected.strip()  # Remove any leading/trailing whitespace
+        selected = selected.strip() 
         for standings_info in st.session_state.standings_array:
             full_name = f"{standings_info['given_name']} {standings_info['family_name']}".strip()
-            if selected == full_name:  # Match the full name exactly
+            if selected == full_name:  
                 position = standings_info["position"]
                 points = standings_info["points"]
                 wins = standings_info["wins"]
                 constructor = standings_info["constructor_name"]
-                # Add driver and position as a dictionary to the list
+                
                 selected_standings_info.append({"Position": position, "Driver": selected, "Constructor": constructor, "Points": points, "Wins": wins})
 
-    # Sort the selected drivers by position (ascending order)
     selected_drivers_info_sorted = sorted(selected_standings_info, key=lambda x: int(x["Position"]))
     df = pd.DataFrame(selected_drivers_info_sorted)
 
-    # Display the selected drivers and their positions in a table format
     if selected_drivers_info_sorted:
         st.header("Standings:")
         st.dataframe(df.set_index(df.columns[0]))
 
+    ######### Q1 #########
+
     selected_qualifying_info = []
 
-    # Iterate through the selected drivers
     for selected in selected_drivers:
-        selected = selected.strip()  # Remove any leading/trailing whitespace
+        selected = selected.strip()  
         for qualifying_info in st.session_state.qualifying_array:
             full_name = f"{qualifying_info['given_name']} {qualifying_info['family_name']}".strip()
-            if selected == full_name:  # Match the full name exactly
+            if selected == full_name: 
                 q1 = qualifying_info["q1"]
                 q2 = qualifying_info["q2"]
                 q3 = qualifying_info["q3"]
@@ -68,7 +76,7 @@ def main():
                 circuit_name = qualifying_info["circuit_name"]
                 date = qualifying_info["date"]
                 constructor = qualifying_info["constructor_name"]
-                # Add driver and position as a dictionary to the list
+
                 selected_qualifying_info.append({"Driver": selected, 
                                         "Constructor": constructor, 
                                         "q1": q1, 
@@ -79,7 +87,6 @@ def main():
                                         "race_name": race_name, 
                                         "circuit_name": circuit_name,
                                         "date": date})
-    # Prepare data
     round_array = []
     q1_lap_time_milliseconds_array = []
     q1_lap_time_array = []
@@ -101,7 +108,6 @@ def main():
     for race in selected_qualifying_info:
         race_array.append(race["race_name"])
 
-    # Create DataFrame
     chart_data = pd.DataFrame(
         {
             "Round": round_array,
@@ -112,10 +118,8 @@ def main():
         }
     )
 
-    # Display the chart with Altair
     st.header("Best Qualifying Times per Driver (Q1)")
 
-    # Create the chart using Altair
     chart = alt.Chart(chart_data).mark_line().encode(
         alt.X('Round', sort='ascending').scale(zero=False),
         alt.Y('Q1 Lap Time (ms)').scale(zero=False),
@@ -123,7 +127,6 @@ def main():
         tooltip=['Round', 'Grand Prix', 'Driver', 'Q1 Lap Time']
     ).interactive().properties(width=1200, height=500)
 
-    # Display the chart
     st.altair_chart(chart)
 
 main()
